761e2bf7429cee79a7cede83dd69a61b52a65c99
Mark eM <Monter@users.noreply.github.com>
2021-12-31 10:23:25 +0000

    Change display errors directive
    On the production server, the error display should be turned off

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
 // errors
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+ini_set('log_errors', 1);
 error_reporting(E_ALL);
 
 // config
